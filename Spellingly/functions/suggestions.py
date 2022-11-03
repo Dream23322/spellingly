d07d033b16a401feafbee18f887ba22ba3ce58a3
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2022-11-03 13:01:05 +1000

    For main.py suggestions

---
+++ Spellingly/functions/suggestions.py
@@ -0,0 +1,19 @@
+def suggestions1():
+    if text == "Hello, how are you?":
+        print("'Hello! How are you today?' might sound better!")
+    
+def wordSuggestions():
+    if word1.lower() == "thi":
+        print("Do you mean: 'the'?")
+    elif word1.lower() == 'thr':
+        print('Do you mean: the?')
+    elif word1.lower() == 'thw':
+        print('Do you mean: the?')
+    elif word1.lower() == 'ther':
+        print("Do you mean: there?")
+    elif word1.lower() == 'thre':
+        print("Do you mean: there?")
+    elif word1.lower() == 'wat':
+        print("Do you mean: what?")
+
+        
