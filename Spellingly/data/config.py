e29d844f8d4b861d3f9dafdb600934f7224675b8
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2022-12-06 07:49:40 +1000

    Add files via upload

---
+++ Spellingly/data/config.py
@@ -0,0 +1,11 @@
+
+# Make sur to have the first letter of True and False as a capital NOT: true false   YES: True False
+
+
+spellCheck = {
+    "enabled": True,
+    "checkForSymbols": True,
+}
+capitalCheck = {
+    "enabled": True,
+}
